c16452ea0b4656ce76357f7888abff1a87d16e78
mipri25 <mipri@gmx.net>
2026-02-09 20:50:46 +0100

    Build Variable angepasst.

--- ccu-historian/src/mdz/ccuhistorian/Main.groovy
+++ ccu-historian/src/mdz/ccuhistorian/Main.groovy
@@ -24,7 +24,7 @@ import mdz.hc.persistence.Storage
 
 class Main {
 
-	public static String version='4.1.3.0_MiPri'
+	public static String version='4.1.3.1_MiPri'
 	
 	private static final Logger log=Logger.getLogger(Main.class.name)
 	private static Main main
